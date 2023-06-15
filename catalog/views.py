e57da3351edada465ce9da204ef3a56af27938a6
Mark Pochivalov <pochivalov.mark@gmail.com>
2023-06-15 14:17:28 +0600

    Task 4 realisation with additional task

--- catalog/views.py
+++ catalog/views.py
@@ -5,4 +5,9 @@ def index(request):
     return render(request, 'catalog/index.html')
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'{name} ({email}): {message}')
     return render(request, 'catalog/contacts.html')
