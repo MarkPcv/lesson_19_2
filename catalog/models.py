b212051175e2948a943322cad1c47cf82ae92393
Mark Pochivalov <pochivalov.mark@gmail.com>
2023-06-23 06:42:57 +0600

    Task 3-4: create models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {
+    'null': True,
+    'blank': True,
+}
+
+class Category(models.Model):
+    name = models.CharField(max_length=250, verbose_name='Category')
+    description = models.TextField(verbose_name='Description')
+
+    def __str__(self):
+        return f'{self.name} ({self.description})'
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+class Product(models.Model):
+
+    name = models.CharField(max_length=250, verbose_name='Name')
+    description = models.TextField(**NULLABLE, verbose_name='Description')
+    photo = models.ImageField(upload_to='shop/',**NULLABLE, verbose_name='Photo')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Category')
+    price = models.IntegerField(verbose_name='Price')
+    date_created = models.DateTimeField(verbose_name='Date Created')
+    date_modified = models.DateTimeField(**NULLABLE, verbose_name='Date Modified')
+
+    def __str__(self):
+        return f'{self.name} (Price: {self.price}): {self.description}'
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
