9588312caa81f655431d6bb60c63bf7961d7e1cb
Mark Pochivalov <pochivalov.mark@gmail.com>
2023-07-17 10:07:53 +0600

    Task 1: add validation for restricted words

--- catalog/forms.py
+++ catalog/forms.py
@@ -4,7 +4,26 @@ from catalog.models import Product
 
 
 class ProductForm(forms.ModelForm):
+
+    restricted_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево',
+                        'бесплатно', 'обман', 'полиция', 'радар']
+
     class Meta:
         model = Product
-        exclude = ('date_created', 'date_modified',)
+        exclude = ('date_modified',)
+
+    def clean_name(self):
+        cleaned_data = self.cleaned_data['name']
+        if cleaned_data.lower() in self.restricted_words:
+            raise forms.ValidationError('Данное название нельзя использовать')
+        return cleaned_data
+
+    def clean_description(self):
+        cleaned_data = self.cleaned_data['description']
+        data_by_word = cleaned_data.lower().split()
+        for word in data_by_word:
+            if word in self.restricted_words:
+                raise forms.ValidationError(
+                    'Данное описание содержит запрещённые слова')
+        return cleaned_data
 
