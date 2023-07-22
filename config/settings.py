31a08c0341dd263ff411f9e8f01ccf33f2f5f273
Mark Pochivalov <pochivalov.mark@gmail.com>
2023-07-22 08:15:15 +0300

    Task 1: add Redis cache service

--- config/settings.py
+++ config/settings.py
@@ -154,3 +154,12 @@ EMAIL_HOST_USER = 'fuckup@oscarbot.ru'
 EMAIL_HOST_PASSWORD = 'AsTSNVv7pun9'
 EMAIL_USE_SSL = True
 
+CACHE_ENABLED = True
+
+if CACHE_ENABLED:
+    CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.redis.RedisCache",
+            "LOCATION": 'redis://127.0.0.1:6379',
+        }
+    }
